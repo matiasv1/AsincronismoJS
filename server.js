377a4977b2f389a24e9d59caa794365cbabc1990
mvargasm <matiasvargas1@hotmail.es>
2023-01-06 13:32:44 -0300

    Server

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require ('express')
+const axios = require ('axios')
+
+const app = express()
+
+async function getConfig(){
+    const url = "http://18.214.103.65:8080/api/auth/login"
+    const data = {"username":"alumnos@alumnos.org", "password":"m7a/s99"}
+    const response = await axios.post(url,data)
+    const token = response.data.token
+    var config = {
+        headers:{
+            Authorization: `Bearer ${token}`
+        }
+    }
+    return config
+}
+
+async function getDataSensors(url, atributo){
+    var config = await getConfig()
+    var response = await axios.get(url,config)
+    console.log(response.data)
+    return response['data'][atributo]
+}
+
+app.get('/sensores_radon', function (req, res){
+    var url = "http://18.214.103.65:8080/api/plugins/telemetry/DEVICE/723d0580-452d-11ed-b4b1-1bcb8f5daa77/values/timeseries?keys=Fecha,Radon&startTs=1265046352083&endTs=1665029708303"
+    getDataSensors(url,'Radon')
+    .then(data => {
+        res.send(data)})
+    .catch(error => {
+        console.log(error)
+        response.send(error)
+    })
+})
+
+app.get('/sensores_multiparametro', function(req,res){
+    console.log("Peticion recibida")
+    var url = 'http://18.214.103.65:8080/api/plugins/telemetry/DEVICE/101d2fe0-454d-11ed-b4b1-1bcb8f5daa77/values/timeseries?keys=TIMESTAMP,WS,WD,Temp,RH,BP,Depth &startTs=1265046352083&endTs=1665043961492'
+    getDataSensors(url,'WD').then (data => res.send(data))
+    
+})
+
+app.listen(3000)
